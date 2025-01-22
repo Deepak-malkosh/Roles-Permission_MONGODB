643b8fa24f58997802cb107905e6fd920df723a3
Deepak Yadav <deepak.yadav@ecosmob.com>
2025-01-22 15:30:15 +0530

    CURD in permission modules

--- helpers/adminValidator.js
+++ helpers/adminValidator.js
@@ -2,4 +2,16 @@ const { check } = require('express-validator');
 
 exports.permissionAddValidator = [
     check('permission_name', 'Permission name is required').not().isEmpty()
+];
+
+
+exports.permissionDeleteValidator = [
+    check('id', 'id is required').not().isEmpty()
+];
+
+
+exports.permissionUpdateValidator = [
+    check('id', 'id is required').not().isEmpty(),
+    check('permission_name', 'Permission name is required').not().isEmpty()
+
 ]
